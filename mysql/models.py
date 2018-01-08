15e63e974ee9db21b0cb416c3513aabfb93a1908
zhuyizhi <zhuyizhi@zofund.com>
2018-01-08 09:27:23 +0800

    sql 展示

--- mysql/models.py
+++ mysql/models.py
@@ -13,6 +13,10 @@ class script(models.Model):
         (3, 'pre'),
         (4, 'prod'),
     )
+    FINISHED_STATE = (
+        (0, 'unfinished'),
+        (1, 'finished'),
+    )
     envr_type = models.IntegerField(choices=ENV_RUN_TYPE, verbose_name="环境类型", default=0)
     name = models.CharField(max_length=255, verbose_name='脚本名称',unique=True)
     tool_script = models.TextField(verbose_name='脚本',null=True, blank=True)
@@ -20,6 +24,7 @@ class script(models.Model):
     comment = models.TextField(verbose_name='说明', null=True, blank=True)
     ctime = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
     utime = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+    is_finished = models.IntegerField(choices=FINISHED_STATE, verbose_name='完成时间', max_length=30,  default=0)
 
     def __str__(self):
         return self.name
--- mysql/urls.py
+++ mysql/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
-from  .import views
+from mysql.views import *
+from . import views
 
 urlpatterns = [
     url(r'^tools.html$', views.tools, name='tools'),
     url(r'^tools-add.html$', views.tools_add, name='tools_add'),
     url(r'^tools-del.html$', views.tools_delete, name='tools_delete'),
-    url(r'^tools-bulk-del.html$', views.tools_bulk_delte, name='tools_bulk_delte'),
+    # url(r'^tools-bulk-del.html$', views.tools_bulk_delte, name='tools_bulk_delte'),
     url(r'^tools-update-(?P<nid>\d+).html$', views.tools_update, name='tools_update'),
+    url(r'^tools/success/$', scripts_success, name='scripts_success'),
 ]
